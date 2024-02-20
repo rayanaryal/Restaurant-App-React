a21d6f35bc05e91f2db1d5663058a64ffd5f99ad
Rajan <rajanaryal248@gmail.com>
2024-02-20 23:02:41 +0000

    Restaurant App

---
+++ src/component/basics/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navbar = ({ filterItem, menuList }) => {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="btn-group">
+          {menuList.map((curElem, index) => {
+            return (
+              <button
+                key={index} // Added a unique key
+                className="btn-group__item"
+                onClick={() => filterItem(curElem)}>
+                {curElem}
+              </button>
+            );
+          })}
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/component/basics/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "https://geekrobocook.com/wp-content/uploads/2021/03/46_Mexican-Maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "Maggi, a popular instant noodle brand, has become a staple in many households globally. Its quick preparation, versatile flavors, and affordability make it a convenient go-to snack or meal. Loved for its comforting taste, Maggi noodles have evolved into a cultural icon, offering a quick and delicious culinary solution.",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "allu pakoida",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Aloo Pakoda, a popular Indian street food, features thinly sliced potatoes coated in a spiced gram flour batter and deep-fried to golden perfection. The crispy exterior gives way to a flavorful and soft interior, making these fritters a delightful snack, often enjoyed with chutneys or as a tea-time treat.",
+    },
+    {
+      id: 3,
+      image: "https://media.post.rvohealth.io/wp-content/uploads/2020/09/AN250-Corn-With-Lime-732x549-thumb-732x549.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Indian-style corn, often served as 'bhutta', is a delectable street food delight. Grilled over open flames and generously coated with a zesty mix of spices, lime, and salt, it transforms into a flavorful and savory treat. This traditional snack perfectly captures the essence of Indian street cuisine, tempting taste buds with every bite.",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Chola, or Chole, is a North Indian dish featuring tender chickpeas cooked in a robust and aromatic tomato-based gravy. Infused with a blend of spices like cumin, coriander, and garam masala, Chola offers a hearty and flavorful experience. It is commonly served with rice, naan, or bhature, reflecting culinary diversity.",
+    },
+    {
+      id: 5,
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsBHpf4viIlT-RMwpwUkUmQpL7RPGcinlqYm6_j9ydqHm9fYMTz3Jj_8IlFvxHjSbNRTE&usqp=CAU",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Pizza, an iconic Italian dish, consists of a thin or thick crust topped with tomato sauce, melted cheese, and various ingredients like pepperoni, vegetables, or herbs. Baked to perfection, it's a globally beloved comfort food, offering a harmonious blend of textures and flavors in every delicious slice.",
+    },
+    {
+      id: 6,
+      image: "https://cookwithrenu.com/wp-content/uploads/2020/09/Rajasthani-Thali_Featured-Image.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "A Non-Veg Thali is a sumptuous Indian meal featuring an assortment of dishes on a single platter. Typically including meat or seafood dishes, such as chicken curry, lamb kebabs, or fish curry, alongside rice, bread, and accompaniments like pickles and raita, it offers a diverse and satisfying culinary experience.",
+    },
+    {
+      id: 7,
+      image: "https://swagathfoods.com.au/cdn/shop/articles/img-indian-sweets-grouped-001_a2fa988b-0458-4a97-b2e8-ca146eec0fb8.jpg?v=1696198780&width=1000",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Indian sweets, or 'mithai', are an integral part of the country's culinary heritage. Varied and indulgent, these desserts encompass a spectrum of flavors and textures, from the syrupy gulab jamun to the delicate rasgulla. Infused with cardamom, saffron, and nuts, Indian sweets are a celebration of cultural richness and culinary artistry.",
+    },
+    {
+      id: 8,
+      image: "https://healthiersteps.com/wp-content/uploads/2022/09/best-rajmachawal.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Rajma rice is a classic North Indian dish, featuring red kidney beans cooked in a thick, aromatic gravy alongside fluffy basmati rice. Infused with a blend of spices, it offers a hearty and nutritious meal, showcasing the rich flavors of Indian cuisine. Rajma rice is a wholesome and satisfying delicacy.",
+    },
+    {
+      id: 9,
+      image: "https://fozias.co.uk/wp-content/uploads/2023/12/Samosas.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Samosa is a beloved Indian snack renowned for its triangular shape and flavorful filling. This crispy pastry typically contains a mixture of spiced potatoes, peas, and occasionally meat. Deep-fried to perfection, samosas are cherished worldwide for their delicious taste, making them a popular choice for snacks or appetizers.",
+    },
+  ];
+  
+  export default Menu;
